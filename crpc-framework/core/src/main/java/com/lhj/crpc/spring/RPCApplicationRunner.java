f03116c1fba2301e83dc414b9ec5cd6c93fdec86
banyan <liuhaojie@stu.cdu.edu.cn>
2023-10-09 19:17:15 +0800

    fix bug 2.0 master

--- crpc-framework/core/src/main/java/com/lhj/crpc/spring/RPCApplicationRunner.java
+++
@@ -1,9 +0,0 @@
-package com.lhj.crpc.spring;
-
-/**
- * @description：
- * @createTime：2023-09-0818:42
- * @author：banyanmei
- */
-//public class RPCApplicationRunner implements ApplicationRunner {
-//}
